fa64c8eead917cb422e73d36446f82f5f7507853
benavern <bencrdc@gmail.com>
2015-08-14 10:13:16 +0200

    pattern limitation

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
 window.onload = function(){
 
-	var taglist  = document.getElementById('test');
-	var taglist2 = document.getElementById('test2');
 
+	var taglist  = document.getElementById('test');
 	var test  = new ZTAGS(taglist,  ["Yolo!", "with", "style!"]);
-	var test2 = new ZTAGS(taglist2, ["Read", "only!"], true);
 
 	var getTags = document.getElementById('get-tags');
 	getTags.addEventListener('click', function(){
@@ -12,5 +10,15 @@ window.onload = function(){
 		alert( JSON.stringify( test.get() ) );
 
 	})
+
+
+	var taglist2 = document.getElementById('test2');
+	var test2 = new ZTAGS(taglist2, ["Read", "only!"], true);
+	
+
+	var taglist3 = document.getElementById('test3');
+	var fruits = ["apple", "cherry", "lemon"];
+	var test3 = new ZTAGS(taglist3, ["apple", "cherry"], null, fruits);
+
 
 };
--- z-tags/z-tags.js
+++ z-tags/z-tags.js
@@ -1,10 +1,11 @@
-var ZTAGS = function (el, predefined, readonly){
+var ZTAGS = function (el, predefined, readonly, pattern){
 
 	// args
 	this.el = el;
 	this.predefined = predefined;
-	
 	this.readonly = readonly;
+	this.pattern = pattern;
+
 	this.listSection;
 	this.addSection;
 
@@ -28,23 +29,26 @@ ZTAGS.prototype.init = function(){
 		that.readonly = false;
 	}
 
+	// patern
+	if (!Array.isArray(that.pattern)){
+		that.pattern = false;
+	}
+
 	// list section
 	that.listSection = document.createElement("div");
 	that.listSection.className = "list-section";
 	that.el.appendChild(that.listSection);
 
 	if (that.readonly === false){
-
 		// add section
 		that.addSection = document.createElement("input");
 		that.addSection.className = "add-section";
 		that.el.appendChild(that.addSection);
 
-		// adding hability
+		// key actions
 		that.addSection.addEventListener('keydown', function (e) {
-	
+
 			var key = e.which || e.keyCode;
-			console.log(key)
 			if (key === 13 || key === 32 || key == 44 || key == 188 ) { // if enter, space or coma
 				e.preventDefault();
 				var newTag = that.addSection.value;
@@ -52,72 +56,66 @@ ZTAGS.prototype.init = function(){
 					that.add(newTag);
 					that.addSection.value = "";
 				}
-
 			}
 			else if(key === 27){
-
 				// escape = blur
 				that.addSection.blur();
 				return false;
-
 			}
 		});
 
 		// display it?
 		that.el.addEventListener('click', function(e){
-
 			if (e.target == this) { 
 				that.addSection.focus();
 			}
-
 		})
 
 	}
 
 	// adds the predefined tags
 	for(var i=0; i < that.predefined.length; i++){
-
 		that.add(that.predefined[i]);
-
 	}
-
-
 
 }
 
 ZTAGS.prototype.add = function(theTagToAdd){
 
 	var that = this;
+	
+	// if there is a pattern and the word doesn't match
+	if(that.pattern !== false && that.pattern.indexOf(theTagToAdd) == -1 ){
+		return ;
+	}
+	else{
 
-	// create the tag Dom obect
-	var tagWrapper = document.createElement("div");
-	var tagLabel = document.createElement("span");
-	var tagName = document.createTextNode(theTagToAdd);
-	tagWrapper.className = "tag-wrapper";
-	tagLabel.className = "tag-label";
-	tagLabel.appendChild(tagName);
-	tagWrapper.appendChild(tagLabel);
+		// create the tag Dom obect
+		var tagWrapper = document.createElement("div");
+		var tagLabel = document.createElement("span");
+		var tagName = document.createTextNode(theTagToAdd);
+		tagWrapper.className = "tag-wrapper";
+		tagLabel.className = "tag-label";
+		tagLabel.appendChild(tagName);
+		tagWrapper.appendChild(tagLabel);
 
-	if(that.readonly === false){
+		if(that.readonly === false){
+			// add it the deletion btn
+			var deleteBtn = document.createElement("span");
+			var deleteLabel = document.createTextNode("X");
+			deleteBtn.className = "tag-delete-btn";
+			deleteBtn.appendChild(deleteLabel);
+			tagWrapper.appendChild(deleteBtn);
+			// event listener
+			deleteBtn.addEventListener('click', function(){
+				that.delete(tagWrapper);
+			})
+		}
 
-		// add it the deletion btn
-		var deleteBtn = document.createElement("span");
-		var deleteLabel = document.createTextNode("X");
-		deleteBtn.className = "tag-delete-btn";
-		deleteBtn.appendChild(deleteLabel);
-		tagWrapper.appendChild(deleteBtn);
-		// event listener
-		deleteBtn.addEventListener('click', function(){
-				
-			that.delete(tagWrapper);
-
-		})
+		// add it to the DOM
+		that.listSection.appendChild(tagWrapper);
 
 	}
-
-	// add it to the DOM
-	that.listSection.appendChild(tagWrapper);
-
 }
 
 ZTAGS.prototype.delete = function(theTagToDelete){
@@ -126,7 +124,6 @@ ZTAGS.prototype.delete = function(theTagToDelete){
 
 	// delete the element
 	theTagToDelete.parentNode.removeChild(theTagToDelete);
-
 }
 
 
@@ -136,15 +133,10 @@ ZTAGS.prototype.get = function(){
 	var tags = [];
 
 	for (var i=0; i<that.listSection.childNodes.length; i++){
-
 		var tag = that.listSection.childNodes[i];
 		var tagName = tag.getElementsByClassName('tag-label')[0].innerHTML;
 		tags.push(tagName);
-
 	}
 
 	return tags;
-	
 }
-
-
